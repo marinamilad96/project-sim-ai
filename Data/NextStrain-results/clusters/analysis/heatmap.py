7cf8c74af5c12afcf061f4adf2bf9d8dd3075c52
marina.milad96 <marina.ayoub765@gmail.com>
2025-09-09 14:02:49 +0200

    adding xml files

---
+++ Data/NextStrain-results/clusters/analysis/heatmap.py
@@ -0,0 +1,84 @@
+
+print("""
+      
+      This script computes the distance matrix from a multiple sequence alignment (MSA) using the BioPython library.
+It uses the identity model to calculate distances, which is suitable for nucleotide sequences.
+It reads a FASTA file containing the MSA, computes the distance matrix, and prints it.
+      
+      
+      """)
+
+import pandas as pd
+import numpy as np
+import os
+import glob
+import seaborn as sns
+import yaml
+import matplotlib.pyplot as plt
+from Bio import AlignIO
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+
+
+with open("/Users/MiladM-Dev/Documents/1PhD/project-1-N450/project-1.1-gendata/Nextstrain/trial-1/results/clusters/analysis/config.yaml") as f:
+    config = yaml.safe_load(f)
+
+#path ='/Users/MiladM-Dev/Documents/1PhD/project-1-N450/project-1.1-gendata/Nextstrain/trial-1/results/clusters/data/aligned_'
+# Load your MSA
+alignment = AlignIO.read(config['paths']['input_path']+ config['file_name']+ ".fasta", "fasta")
+
+# Create a DistanceCalculator object using the "identity" model, which is essentially p-distance
+calculator = DistanceCalculator("identity")
+
+# Compute the distance matrix
+dm = calculator.get_distance(alignment)
+
+print( """
+      
+      The distance matrix is a square matrix where the element at (i, j) represents the distance between sequences i and j.
+The names of the sequences are stored in the 'names' attribute of the distance matrix object.
+      
+      """
+)# Print the distance matrix
+
+
+
+"""Converting the distance matrix to a lower triangular matrix.
+This is useful for visualizing the distances in a heatmap format, where only the lower triangle is filled with values."""
+
+# Extract labels from the distance matrix
+labels = dm.names  
+
+# Build lower triangular matrix from the above dictionary
+n = len(labels)
+mat = np.zeros((n, n))
+
+# Fill lower triangle with your data
+for col_idx, label in enumerate(labels):
+    vals = dm[label]
+    for row_idx, val in enumerate(vals):
+        if row_idx <= col_idx:  # Ensure only lower triangle is filled
+            mat[col_idx, row_idx] = val
+        else:
+            mat[col_idx, row_idx] = np.nan  # Fill upper triangle with NaN or zeros
+
+
+
+# Create pandas DataFrame with row and column labels
+df_matrix = pd.DataFrame(mat, index=labels, columns=labels)
+df_matrix.to_csv(config['paths']['output_path'] + config['file_name'] + "_distance_matrix.csv")
+
+print("""
+      
+      Visualizing the distance matrix as a heatmap using seaborn.
+This provides a graphical representation of the distances between sequences, where colors indicate the distance values.
+      Saving it as PNG file.
+      
+      """
+)
+# Show the matrix as a heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(df_matrix, cmap="viridis", xticklabels=True, yticklabels=True)
+plt.title("Distance Matrix Heatmap" + " - " + config['file_name'])
+plt.tight_layout()
+plt.savefig(config['paths']['output_path'] + config['file_name'] + "_distance_matrix_plot.png")
+#plt.show()
