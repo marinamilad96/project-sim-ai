b32ed9c7076f756e22ce98a2208ac62885cbf72c
marinamilad96 <marina.ayoub765@gmail.com>
2025-09-09 13:10:20 +0200

    Box plot full and sampled trees

--- beast-simulation/Scripts/generate-plots/box-h-l-df-plot.py
+++ beast-simulation/Scripts/generate-plots/box-h-l-df-plot.py
@@ -26,8 +26,9 @@ df[['name', 'seed', "tree_type"]] = df['file_name'].str.extract(
 df = df[['file_name', 'name', 'seed', "tree_type", 'tree_height', 'tree_length']]
 df.reset_index(drop=True, inplace=True)
 
-df.to_csv(os.path.join(
-    os.path.expandvars(config["FilePath"]["outputFilePath"]), "height_length_DataFrame.csv"), index=False)
+
+df.to_csv(os.path.join(os.path.expandvars(config["FilePath"]["outputFilePath"]), "height_length_DataFrame.csv"), index=False)
+
 print("Final DataFrame is saved:", os.path.join(
     os.path.expandvars(config["FilePath"]["outputFilePath"]), "height_length_DataFrame.csv"))
 
@@ -55,6 +56,7 @@ fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(
 # Collect unique names
 names = df["name"].unique()
 
+# --- FULL TREES ---
 # --- Tree Height ---
 for i, name in enumerate(names):
     # Group by name only
@@ -73,7 +75,7 @@ axs[0].set_title("Box plots of full Tree Height by Name")
 axs[0].set_ylabel("Tree Height")
 
 
-# --- Tree Length full trees ---
+# --- Tree Length ---
 for i, name in enumerate(names):
     # Group by name only
     group = full_df[full_df["name"] == name]
@@ -96,4 +98,49 @@ plt.savefig(os.path.join(
     os.path.expandvars(config["FilePath"]["outputFilePath"]), "Box_plot_Full_Tree_Height_Length.png"))
 print("Box plots saved:",
       os.path.join(os.path.expandvars(config["FilePath"]["outputFilePath"]), "Box_plot_Full_Tree_Height_Length.png"))
+plt.show()
+
+
+# --- SAMPLED TREES ---
+# --- Tree Height ---
+for i, name in enumerate(names):
+    # Group by name only
+    group = sampled_df[sampled_df["name"] == name]
+    if group.empty:
+        continue
+    axs[0].boxplot(
+        x=group["tree_height"].dropna(),
+        positions=[i]
+    )
+
+# Set x-ticks at each name position
+axs[0].set_xticks(range(len(names)))
+axs[0].set_xticklabels(names, rotation=45, ha="right")
+axs[0].set_title("Box plots of sampled Tree Height by Name")
+axs[0].set_ylabel("Tree Height")
+
+
+# --- Tree Length ---
+for i, name in enumerate(names):
+    # Group by name only
+    group = sampled_df[sampled_df["name"] == name]
+    if group.empty:
+        continue
+    axs[1].boxplot(
+        x=group["tree_length"].dropna(),
+        positions=[i]
+    )
+
+# Set x-ticks at each name position
+axs[1].set_xticks(range(len(names)))
+axs[1].set_xticklabels(names, rotation=45, ha="right")
+axs[1].set_title("Box plots of sampled Tree length by Name")
+axs[1].set_ylabel("Tree length")
+
+
+plt.tight_layout()
+plt.savefig(os.path.join(
+    os.path.expandvars(config["FilePath"]["outputFilePath"]), "Box_plot_sampled_Tree_Height_Length.png"))
+print("Box plots saved:",
+      os.path.join(os.path.expandvars(config["FilePath"]["outputFilePath"]), "Box_plot_sampled_Tree_Height_Length.png"))
 plt.show()
